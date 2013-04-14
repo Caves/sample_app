a2e0d01d31bfe4956bc6e820fcd3de52cddd698d
Andreas Haraldsson <cavesq@gmail.com>
2013-04-14 18:50:19 +0200

    App Helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,14 +4,14 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the h1 'Sample App'" do
+    it "should have the h1 'Home'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => "Home")
     end
     
     it "should have the title 'Home'" do
       visit "/static_pages/home"
-      page.should have_selector('title', :text => "| Home")
+      page.should have_selector('title', :text => "Sample")
     end
   end
   
